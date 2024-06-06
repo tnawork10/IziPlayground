8f47a197d65fa0cc9fa479385ad3288ea55d7907
Chan Ivan <i.chan@matriks.group>
2024-06-06 17:31:10 +0300

    fix: *.nuspec

--- QuickTest/Program.cs
+++ QuickTest/Program.cs
@@ -1,24 +1,21 @@
-﻿using System.Text.Json.Nodes;
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text.Json.Nodes;
 using IziHardGames.IziAsyncEnumerables;
 using Microsoft.Extensions.DependencyInjection;
 using Moq;
 using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Builder;
 
-var json = "[\n  {\n    \"hardwareId\": 0,\n    \"signalId\": 0\n  }\n]";
 
-if(Enumerable.Range(0, 2).Contains(2))
- {
-        Console.WriteLine("Fire");
- }
-Console.WriteLine("Hello, World!");
-JsonArray obj = JsonObject.Parse(json).AsArray();
+await TestAsync();
 
-foreach (var VARIABLE in obj)
+
+partial class Program
 {
-    Console.WriteLine(VARIABLE["hardwareId"].GetValue<long>());
-    Console.WriteLine(VARIABLE["signalId"].GetValue<long>());
+    public static async Task TestAsync()
+    {
+        var v = ValueTask.FromResult(100f);
+        Console.WriteLine(v.GetType().GenericTypeArguments.First().FullName);
+    }
 }
-
-
-Console.WriteLine("Complete");
