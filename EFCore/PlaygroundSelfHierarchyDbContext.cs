e26e4c8668622f344a52a51046f5d369bac2362b
Chan Ivan <cna@gce.ru>
2024-10-15 19:43:10 +0300

    *ef core

---
+++ EFCore/PlaygroundSelfHierarchyDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+using Microsoft.EntityFrameworkCore;
+
+namespace IziHardGames.Playgrounds.ForEfCore
+{
+    public class PlaygroundSelfHierarchyDbContext : DbContext
+    {
+        public DbSet<EntityHierarchy> Hierarchies { get; set; }
+
+        public PlaygroundSelfHierarchyDbContext(DbContextOptions<PlaygroundSelfHierarchyDbContext> options) : base(options)
+        {
+
+        }
+    }
+
+
+    public class EntityHierarchy
+    {
+        [Key]
+        public Guid Guid { get; set; } = Guid.NewGuid();
+        public Guid? ParentGuid { get; set; }
+        [JsonIgnore] public EntityHierarchy? Parent { get; set; }
+        public ICollection<EntityHierarchy>? Childs { get; set; }
+    }
+}
--- QuickTest/Program.cs
+++ QuickTest/Program.cs
@@ -1,29 +1,35 @@
-﻿using System.Reflection;
+﻿using System.Collections.Concurrent;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
 using System.Runtime.CompilerServices;
 using System.Text.Json.Nodes;
-using IziHardGames.IziAsyncEnumerables;
-using Microsoft.Extensions.DependencyInjection;
-using Moq;
-using Microsoft.Extensions.Logging;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.EntityFrameworkCore;
-using IziHardGames.Playgrounds.ForEfCore;
-using IziHardGames.ForUnsafe;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
 
 namespace QuickTest;
 
-if(Enumerable.Range(0, 2).Contains(2))
- {
-        Console.WriteLine("Fire");
- }
-Console.WriteLine("Hello, World!");
-JsonArray obj = JsonObject.Parse(json).AsArray();
-
-foreach (var VARIABLE in obj)
+class Program
 {
-    Console.WriteLine(VARIABLE["hardwareId"].GetValue<long>());
-    Console.WriteLine(VARIABLE["signalId"].GetValue<long>());
+    static async Task Main(string[] args)
+    {
+        var list = new int[] { 1 };
+        var order = list.OrderBy(x => x).ToArray();
+
+    }
+
+    private static async IAsyncEnumerable<int> AsyncStream([EnumeratorCancellation] CancellationToken ct = default)
+    {
+        yield return 0;
+        throw new NotImplementedException();
+    }
+
+
+    public static IEnumerable<(string, string)> Parse(QueryString queryString)
+    {
+        var pairs = Microsoft.AspNetCore.WebUtilities.QueryHelpers.ParseQuery(queryString.Value);
+        foreach (var item in pairs)
+        {
+            yield return (item.Key, item.Value);
+        }
+    }
 }
-
-
-Console.WriteLine("Complete");
