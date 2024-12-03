d9345ad56e51f2f5151db9eb98e527517d0b0d8e
Chan Ivan <cna@gce.ru>
2024-12-03 12:18:53 +0300

    + dynamicexpresso

--- QuickTest/Program.cs
+++ QuickTest/Program.cs
@@ -2,9 +2,12 @@ using System.Collections.Concurrent;
 using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using System.Runtime.CompilerServices;
+using System.Text;
 using System.Text.Json;
 using System.Text.Json.Nodes;
+using System.Text.RegularExpressions;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.FileSystemGlobbing.Internal;
 using Microsoft.Extensions.Primitives;
 
 namespace QuickTest;
@@ -12,12 +15,69 @@ class Program
 {
     static async Task Main(string[] args)
     {
-        var date = new DateTime(2024, 12, 30);
-        Console.WriteLine(DateOnly.FromDateTime(date).ToString());
-        Console.WriteLine(JsonSerializer.Serialize(DateOnly.FromDateTime(date)));
+        Console.WriteLine(JsonSerializer.Serialize(TimeSpan.MaxValue));
+        var reg = new RegExReplaceForExcelFormula();
+        var v2 = new ReplaceForExcelFormula();
 
-        await ParallelForEach.RunForeachToArrayWrite();
+        var input = "stack —“≈œ≈Õ‹(3,—“≈œ≈Õ‹(5,2))asd asd +asd a=do\r\nasda —“≈œ≈Õ‹(3,2+asda) asda\r\nasdassd —“≈œ≈Õ‹(3,—“≈œ≈Õ‹(5,2))aaaa ()asldklak\r\n—“≈œ≈Õ‹(3,—“≈œ≈Õ‹(—“≈œ≈Õ‹((1+1),2),2)) +++ ()asd ()";
+        //var input = "—“≈œ≈Õ‹(3,2)";
+        var res = reg.ReplaceMathPow(input);
+        var res2 = v2.Replace(input);
+        Console.WriteLine(res2);
+    }
+        
+    public class ReplaceForExcelFormula
+    {
+        public const string SEQ_OPEN = "—“≈œ≈Õ‹(";
+        public const string SEQ_CLOSE = ")";
+        public const char SKIP_OPEN = '(';
+        public const char SKIP_CLOSE = ')';
+        public static readonly Regex regex = new Regex(@"—“≈œ≈Õ‹\(", RegexOptions.Singleline);
+        public const string REPLACE_POW = "Math.Pow(";
+
+        public string Replace(string input)
+        {
+            return regex.Replace(input, REPLACE_POW);
+        }
     }
 
+    public class RegExReplaceForExcelFormula
+    {
+        public const string FUNC_NAME_MATH_POW = "—“≈œ≈Õ‹";
+        public readonly Regex regex = new Regex(PATTERN_MATH_POW, RegexOptions.Singleline | RegexOptions.IgnorePatternWhitespace);
+        public const string PATTERN_MATH_POW =
+            @"(?<pow>
+                (?<openPow>—“≈œ≈Õ‹\(
+                )+
+                (?<powinside>
+                    [^()]*
+                    (?<nestedPara>
+                        (?<paraLeft>
+                            (?<Open>\()
+                            [^()]*
+                        )+
+                        (?<paraRight>
+                            (?<BetweenPara-Open>\))
+                            [^()]*
+                        )+
+                    )*
+                )
+                (?<betweenPow-openPow>\))+
+            )";
 
+        public string ReplaceMathPow(string input)
+        {
+
+            var mathces = regex.Matches(input) as IEnumerable<Match>;
+            // ÔÓ ÛÏÓÎ˜‡ÌË˛ mathces ÓÚÒÚÓÚËÓ‚‡Ì ÔÓ ËÌ‰ÂÍÒÛ ‚ Ì‡Ô‡‚ÎÂÌËË ÔÓËÒÍ‡ ÚÂÍÒÚÂ (ÒÎÂ‚‡ Ì‡Ô‡‚Ó)
+            foreach (var match in mathces)
+            {
+                var grps = match.Groups;
+                var betweenPow = grps.Values.FirstOrDefault(x => x.Name == "betweenPow");
+                ArgumentNullException.ThrowIfNull(betweenPow);
+                var span = betweenPow.ValueSpan;
+            }
+            return "";
+        }
+    }
 }
---
+++ playground/DynamicExpresso/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Text.Json;
+
+namespace DynamicExpresso
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            var result = 10.34601130382;
+            var json = await File.ReadAllTextAsync(@"C:\Users\adyco\Documents\[Personal] CSharp\IziPlayground\playground\DynamicExpresso\input.json");
+            Dictionary<string, double?> formulaParameterNames = JsonSerializer.Deserialize<Dictionary<string, double?>>(json);
+            var target = new Interpreter();
+            target.SetDefaultNumberType(DefaultNumberType.Double);
+            var parameters = formulaParameterNames.Select(x => new Parameter(x.Key, typeof(double))).ToArray();
+            string formulaExpression = @"((((var0/1000)-((var1/1000)*(((var2/1000)+(var3/1000)-(var4/1000))/((var5/1000)+(var6/1000)+(var7/1000)-(var8/1000)))))+((var9/1000)*(((var10/1000)+(var11/1000)-(var12/1000))/((var13/1000)+(var14/1000)+(var15/1000)-(var16/1000)))))+(((var17/1000)-((var18/1000)*(((var19/1000)+(var20/1000)-(var21/1000))/((var22/1000)+(var23/1000)+(var24/1000)-(var25/1000)))))+((var26/1000)*(((var27/1000)+(var28/1000)-(var29/1000))/((var30/1000)+(var31/1000)+(var32/1000)-(var33/1000)))))+((var34/1000)-(var35/1000)-(var36/1000)-(var37/1000)-(var38/1000)+(var39/1000))+(((var40/1000)-(var41/1000)*(((var42/1000)+(var43/1000)-(var44/1000))/((var45/1000)+(var46/1000)+(var47/1000)-(var48/1000))))+((var49/1000)*(((var50/1000)+(var51/1000)-(var52/1000))/((var53/1000)+(var54/1000)+(var55/1000)-(var56/1000)))+(var57/1000))))+(((var58/1000)))+((((var59/1000)-(0))+((var60/1000)*(((var61/1000)+(var62/1000)-(var63/1000))/((var64/1000)+(var65/1000)+(var66/1000)-(var67/1000))))))";
+            Lambda l1 = target.Parse("Math.Pow(2,2)");
+            var val1 = l1.Invoke();
+
+            Lambda lambdaFormulaExpression = target.Parse(formulaExpression, parameters);
+            var parameterValues = formulaParameterNames.Select(x => new Parameter(x.Key, x.Value ?? 0.0)).ToArray();
+            var resultFact = lambdaFormulaExpression.Invoke(parameterValues);
+            //var resultFactDouble = double.Parse((string)resultFact);
+            var resultFactDouble = (double)resultFact;
+            Console.WriteLine(resultFact);
+            Console.WriteLine(Math.Abs(resultFactDouble - result) < double.Epsilon);
+            Console.WriteLine();
+            Pow();
+        }
+
+        public static void Pow()
+        {
+            var perameters = new Parameter[] {
+                new Parameter("Value", typeof(double), 2),
+                new Parameter("Pow", typeof(double),3),
+            };
+            var v = Math.Pow(1, 2);
+            var formulaExpression = $"Math.Pow(Value,Pow)";
+            var target = new Interpreter();
+            Lambda lambdaFormulaExpression = target.Parse(formulaExpression, perameters);
+
+            var resultFact = lambdaFormulaExpression.Invoke(perameters);
+            Console.WriteLine(resultFact);
+
+            string s = $"<EXPORESSION_POW>СТЕПЕНЬ(<EXPORESSION_POW>СТЕПЕНЬ(2;3)</EXPORESSION_POW>;3)</EXPORESSION_POW>";
+
+        }
+    }
+}
