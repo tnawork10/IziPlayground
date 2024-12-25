c087be9b54a701402181e783a8ea6eea890ea6f5
Chan Ivan <cna@gce.ru>
2024-12-25 10:45:25 +0300

    temp

---
+++ QuickTest/RegExReplaceForExcelFormula.cs
@@ -0,0 +1,44 @@
+﻿using System.Text.RegularExpressions;
+
+namespace QuickTest;
+
+
+public class RegExReplaceForExcelFormula
+{
+    public const string FUNC_NAME_MATH_POW = "�������";
+    public readonly Regex regex = new Regex(PATTERN_MATH_POW, RegexOptions.Singleline | RegexOptions.IgnorePatternWhitespace);
+    public const string PATTERN_MATH_POW =
+        @"(?<pow>
+                (?<openPow>�������\(
+                )+
+                (?<powinside>
+                    [^()]*
+                    (?<nestedPara>
+                        (?<paraLeft>
+                            (?<Open>\()
+                            [^()]*
+                        )+
+                        (?<paraRight>
+                            (?<BetweenPara-Open>\))
+                            [^()]*
+                        )+
+                    )*
+                )
+                (?<betweenPow-openPow>\))+
+            )";
+
+    public string ReplaceMathPow(string input)
+    {
+
+        var mathces = regex.Matches(input) as IEnumerable<Match>;
+        // �� ��������� mathces ������������� �� ������� � ����������� ������ ������ (����� �������)
+        foreach (var match in mathces)
+        {
+            var grps = match.Groups;
+            var betweenPow = grps.Values.FirstOrDefault(x => x.Name == "betweenPow");
+            ArgumentNullException.ThrowIfNull(betweenPow);
+            var span = betweenPow.ValueSpan;
+        }
+        return "";
+    }
+}
