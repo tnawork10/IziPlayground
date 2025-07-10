3622fec4d992be53ad0cf3e8ee2cd8b11905bfa3
Tran Ngoc Anh <adycon94@gmail.com>
2025-07-10 17:43:12 +0300

    move ef core add mapped file

---
+++ playground/MemoryMappedFiles/MemoryMappedFiles.Master/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace MemoryMappedFiles.Master
+{
+    using System.IO.MemoryMappedFiles;
+    using System.Text;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using var mmf = MemoryMappedFile.CreateFromFile("/dev/shm/test", System.IO.FileMode.OpenOrCreate, "test", 100_000);
+            using var accessor = mmf.CreateViewAccessor();
+
+            string message = "Hello from Writer!";
+            byte[] buffer = Encoding.UTF8.GetBytes(message);
+            accessor.Write(0, buffer.Length);
+            accessor.WriteArray(4, buffer, 0, buffer.Length);
+
+            Console.WriteLine("Writer wrote: " + message);
+            Console.ReadLine(); // Keep open
+        }
+    }
+}
---
+++ playground/MemoryMappedFiles/MemoryMappedFiles.Slave/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace MemoryMappedFiles.Slave
+{
+    using System.IO.MemoryMappedFiles;
+    using System.Text;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using var mmf = MemoryMappedFile.CreateFromFile("/dev/shm/test", FileMode.Open);
+            using var accessor = mmf.CreateViewAccessor();
+
+            int length = accessor.ReadInt32(0);
+            byte[] buffer = new byte[length];
+            accessor.ReadArray(4, buffer, 0, length);
+
+            string message = Encoding.UTF8.GetString(buffer);
+            Console.WriteLine("Reader read: " + message);
+        }
+    }
+}
